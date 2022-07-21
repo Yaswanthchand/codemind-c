e4e21d0055ae6e58efe743a700661a43d8e28de1
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-07-21 10:52:26 +0530

    #include<stdio.h> int main() {     int a,b,i,res;     scanf("%d%d",&a,&b);     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0&&b%i==0)         {             res=i;         }     }     printf("%d",res); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,24 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int a,b,i,res;
     scanf("%d%d",&a,&b);
-    int h,i,max,min;
-    if (a>b)
-        max=a;
-    else
-        max=b;
-    h=1;
-    for (i=1; i<=max; i++)
+    for(i=1;i<=a&&i<=b;i++)
     {
-       if (a%i==0 && b%i==0)
-       {
-           if (i>h)
-           {
-               h=i;
-           }
-       }
+        if(a%i==0&&b%i==0)
+        {
+            res=i;
+        }
     }
-    printf("%d",h);
-    return 0;
+    printf("%d",res);
 }
